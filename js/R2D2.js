da8d156c6c3d39950155f8bbd3532635b5ce2916
fern_al1 <fern_al1@ccma.cat>
2020-12-17 12:57:32 +0100

    Add highlighting

--- js/R2D2.js
+++ js/R2D2.js
@@ -7,18 +7,26 @@ function cridaServo(e) {
 }
 function printPosition(obj) {
     let position = 2000 - (obj.value * 10);
-    let degrees = obj.value*-1;
+    let degrees = obj.value * -1;
     let servo = parseInt(obj.id.slice(5));//removing servo from id
+    removeAllActive();
+    obj.parentNode.classList.add('active');
     console.log(`Moving servo ${servo} to position ${position} - Aperture of ${obj.value} % `);
     fetch(`/?servo=${servo}&position=${position}`);
-    document.querySelector(".line").style.transform=`rotate(${degrees}deg)`;
+    document.querySelector(".line").style.transform = `rotate(${degrees}deg)`;
 }
 function resetServos() {
     let numServos = servos.length;
-    let zeroPosition=1800;
+    let zeroPosition = 1800;
     console.log(`Reseting servos from ${servos[0]} to ${numServos}`)
     for (let index = 0; index < numServos; index++) {
         console.log(`Reseting servo ${index}`);
-        fetch(`/?servo=${index}&position=${zeroPosition}`);
+        fetch(`/?servo=${index}&position=${zeroPosition}`);      
+    }
+}
+function removeAllActive() {
+    let servosLi = document.querySelectorAll('.servos li');
+    for (let index = 0; index < servosLi.length; index++) {
+        servosLi[index].classList.remove('active');
     }
 }
